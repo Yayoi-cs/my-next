5c38c674879aa84bbbe3965e05712fec02ff7a00
Yayoi-cs <hirogoshawk3249@gmail.com>
2025-02-06 18:04:11 +0900

    update blog data

--- src/app/components/blogData.tsx
+++ src/app/components/blogData.tsx
@@ -1,75 +1,94 @@
 import {blogCardArgs} from "@/app/components/blogCard";
-export const blogData:blogCardArgs[] = [
-     {
+
+export const blogData: blogCardArgs[] = [
+    {
+        title: "upsolve: Kinderheim511",
+        image: "/images/babyHeapExploit.png",
+        description: ["SurdnlenCTF 2025 pwn challenge"],
+        link: "https://yayoi-cs.github.io/blog/kinderheim-511.html"
+    },
+    {
+        title: "upsolve: Snowstorm",
+        image: "/images/babyHeapExploit.png",
+        description: ["SurdnlenCTF 2025 pwn challenge"],
+        link: "https://yayoi-cs.github.io/blog/snowstorm.html"
+    },
+    {
+        title: "upsolve: Anodic Music",
+        image: "/images/babyHeapExploit.png",
+        description: ["SurdnlenCTF 2025 Reversing challenge"],
+        link: "https://yayoi-cs.github.io/blog/anodic-music.html"
+    },
+    {
+        title: "upsolve: The Kudryavka Sequence",
+        image: "/images/babyHeapExploit.png",
+        description: ["IERAE CTF 2024 reversing challenge"],
+        link: "https://yayoi-cs.github.io/blog/the-kudryavka-sequence.html"
+    },
+    {
+        title: "UofT CTF 2025",
+        image: "/images/babyHeapExploit.png",
+        description: ["shellcode challenge"],
+        link: "https://yayoi-cs.github.io/blog/uoft-ctf-2024.html"
+    },
+    {
+        title: "godb",
+        image: "/images/babyHeapExploit.png",
+        description: ["the fastest parallel debugger"],
+        link: "https://yayoi-cs.github.io/blog/godb.html"
+    },
+    {
         title: "Baby heap exploit",
         image: "/images/babyHeapExploit.png",
-        description: ["basic heap exploit with my own chal","libc-leak and got a shell"],
-        link:"https://hackmd.io/@tsuneki/H1RUzEOBkg"
-     },
-     {
-        title: "Intro to SMT Solver",
-        image: "/images/introToSMTSolver.png",
-        description: ["How to SMT","I tried z3-solver with CTF challenges"],
-        link:"https://hackmd.io/@tsuneki/BynzAaIrkx"
-     },
-     {
+        description: ["basic heap exploit with my own chal", "libc-leak and got a shell"],
+        link: "#"
+    },
+    {
         title: "AlpacaHack Round8 Writeup",
         image: "/images/alpaca8_2.png",
-        description: ["reversing challenge","AlpacaHack Round8 WriteUps"],
-        link:"https://hackmd.io/@tsuneki/SJClekl8yg"
+        description: ["reversing challenge", "AlpacaHack Round8 WriteUps"],
+        link: "https://yayoi-cs.github.io/blog/alpacahack-round8.html"
     },
     {
         title: "UrmiaCTF Writeup",
         image: "/images/urmia.png",
-        description: ["pwn:loop-up","UrmiaCTF2024 WriteUps!"],
-        link:"https://hackmd.io/@tsuneki/HyOELF220"
+        description: ["pwn:loop-up", "UrmiaCTF2024 WriteUps!"],
+        link: "https://yayoi-cs.github.io/blog/urmiactf-2024.html"
     },
     {
         title: "LITCTF Writeup",
         image: "/images/pic3.JPG",
-        description: ["function pairing","infinite echo"],
-        link:"https://hackmd.io/@tsuneki/rkCGIIucC"
+        description: ["function pairing", "infinite echo"],
+        link: "https://yayoi-cs.github.io/blog/lit-ctf.html"
     },
     {
         title: "N00bzCTF Writeup",
         image: "/images/noobz.png",
-        description: ["N00bzCTF2024","fun challenges!!"],
-        link:"https://hackmd.io/@tsuneki/SkxTEjptA"
+        description: ["N00bzCTF2024", "fun challenges!!"],
+        link: "https://yayoi-cs.github.io/blog/n00bzctf-2024.html"
     },
     {
         title: "Imaginary CTF Writeup",
         image: "/images/imaginary.png",
-        description: ["Imaginary CTF 2024","fun challenges!!"],
-        link:"https://hackmd.io/@tsuneki/S1DmbWa_R"
+        description: ["Imaginary CTF 2024", "fun challenges!!"],
+        link: "https://yayoi-cs.github.io/blog/imaginary-ctf.html"
     },
     {
         title: "DUCTF Writeup",
         image: "/images/ductf.png",
-        description: ["DUCTF2024 WriteUps!","pwn-vector overflow"],
-        link:"https://hackmd.io/@tsuneki/SJl--TRwwC"
+        description: ["DUCTF2024 WriteUps!", "pwn-vector overflow"],
+        link: "https://yayoi-cs.github.io/blog/helloworld.html"
     },
     {
         title: "UIUCTF Writeup",
         image: "/images/uiuctf.png",
-        description: ["crypto:matrix challenge","UIU CTF 2024 WriteUps!"],
-        link:"https://hackmd.io/@tsuneki/Bk6jdo1w0"
-    },
-    {
-        title: "CTF4b Writeup",
-        image: "/images/ctf4b.png",
-        description: ["seccon beginners 2024","We got 17th place"],
-        link:"https://hackmd.io/@tsuneki/rJg8eehHA"
-    },
-    {
-        title: "DiverOSINT Writeup",
-        image: "/images/diver.png",
-        description: ["Diver OSINT CTF2024 WriteUps!","We got 10th place!"],
-        link:"https://hackmd.io/@YumaOchi/rJ52sqGH0"
+        description: ["crypto:matrix challenge", "UIU CTF 2024 WriteUps!"],
+        link: "https://yayoi-cs.github.io/blog/uiuctf2024.html"
     },
     {
         title: "PicoCTF Writeup",
         image: "/images/pico.png",
-        description: ["PicoCTF2024 WriteUps!","5825pt,top2%","Full score in 2 categories"],
-        link:"https://github.com/Yayoi-cs/PicoCTF2024WriteUps/blob/main/Writerside/topics/OverView.md"
+        description: ["PicoCTF2024 WriteUps!", "5825pt,top2%", "Full score in 2 categories"],
+        link: "https://yayoi-cs.github.io/blog/picoctf2024-writeup.html"
     }
 ]
